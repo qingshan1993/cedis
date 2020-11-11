3647e5186640af2d8d51f7256a4adbcd49985f07
jiangjq <qingjunruguan@yahoo.com>
2020-11-11 17:56:51 +0800

    KMP算法自我想象版

--- src/test/java/com/cedis/algorithm/DMP.java
+++ src/test/java/com/cedis/algorithm/DMP.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
  * @version 1.0
  * @date 2020/11/9
  * @desc dmp算法
+ * @ref http://www.ruanyifeng.com/blog/2013/05/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm.html
+ *      http://jakeboxer.com/blog/2009/12/13/the-knuth-morris-pratt-algorithm-in-my-own-words/
+ *
  */
 public class DMP {
 
@@ -40,5 +43,25 @@ public class DMP {
         }
         return pmt;
     }
+
+     /**
+     *
+     * @param pattern 匹配串
+     * @param src 源串
+     * @return
+     */
+    public static int KMP(String pattern, String src) {
+        int[] partialMatchTable = getPartialMatchTable(pattern);
+        for (int i=0; i< src.length()-pattern.length(); i++) {
+            for (int j=0; j<pattern.length(); j++) {
+                if (pattern.charAt(j) != src.charAt(i)) {
+                    //
+
+                }
+            }
+        }
+        return 1;
+    }
+
     
  }
