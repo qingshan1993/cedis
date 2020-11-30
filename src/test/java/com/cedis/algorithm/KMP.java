190765c65d1507a3234e6b2a48b8a50bdc6c3efb
jiangjq <qingjunruguan@yahoo.com>
2020-11-30 21:19:11 +0800

    kmp算法再领悟

--- src/test/java/com/cedis/algorithm/KMP.java
+++ src/test/java/com/cedis/algorithm/KMP.java
@@ -131,6 +131,51 @@ public class KMP {
         System.out.println("KMP:" + kmp);
     }
 
+     protected static  int[] getNext(char[] p) {
+        int pLen = p.length;
+        int[] next = new int[pLen];
+        int k = -1;
+        int j = 0;
+        next[0] = -1; // next数组中next[0]为-1
+        while (j < pLen - 1) {
+            if (k == -1 || p[j] == p[k]) {
+                k++;
+                j++;
+                next[j] = k;
+            } else {
+                k = next[k];
+            }
+        }
+        return next;
+    }
+
+    @Test
+    public void  getNextTest() {
+        char[] p = "ABCABDABCD".toCharArray();
+        int[] next = getNext(p);
+        System.out.println(Arrays.toString(next));//[-1, 0, 0, 1, 2, 3, 4]
+    }
+
+    public static int indexOf(String source, String pattern) {
+        int i = 0, j = 0;
+        char[] src = source.toCharArray();
+        char[] ptn = pattern.toCharArray();
+        int sLen = src.length;
+        int pLen = ptn.length;
+        int[] next = getNext(ptn);
+        while (i < sLen && j < pLen) {
+            // 如果j = -1,或者当前字符匹配成功(src[i] = ptn[j]),都让i++,j++
+            if (j == -1 || src[i] == ptn[j]) {
+                i++;
+                j++;
+            } else {
+                // 如果j!=-1且当前字符匹配失败,则令i不变,j=next[j],即让pattern模式串右移j-next[j]个单位
+                j = next[j];
+            }
+        }
+        return  j == pLen ? i - j : -1;
+    }
+
 
 
 }
