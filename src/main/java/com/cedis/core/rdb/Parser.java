9a1a347f4e473b7b329b3c419dc6659e8d183d56
qingjun6093 <2916147177@qq.com>
2022-03-05 23:37:17 +0800

    nio读取文件学习

--- src/main/java/com/cedis/core/rdb/Parser.java
+++ src/main/java/com/cedis/core/rdb/Parser.java
@@ -1,6 +1,14 @@
 package com.cedis.core.rdb;
 
 import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author jjq
@@ -10,8 +18,17 @@ import java.io.ByteArrayInputStream;
  */
 public class Parser {
 
-    private ByteArrayInputStream rdb;
+    private String filePath;
 
+    private FileChannel fileChannel;
+
+    private ByteBuffer byteBuffer;
+
+    public Parser(String filePath) throws FileNotFoundException {
+        this.filePath = filePath;
+        this.byteBuffer = ByteBuffer.allocate(1024);
+        this.fileChannel = new RandomAccessFile(filePath,"r").getChannel();
+    }
 
     /**
      * 验证rdb文件某一段byte数组是否和expect字符串相等
@@ -23,5 +40,38 @@ public class Parser {
     private void verifyStringEqual(int start, int length, String expect) {
 
 
+    }
+
+    /**
+     * 读取文件流,返回指定区间的byte数组
+     * @param start
+     * @param length -1将不限制长度,0将抛出异常
+     * @return
+     */
+    public byte[] readBytes(int start, int length) {
+        if (start<0 || length == 0 || length < -1) {
+            throw new IllegalArgumentException("variable 'start' value must >= 0;" +
+                    "variable 'length' value must >0 or ==-1.");
+        }
+        byte[] result = null;
+        if (length>0) {
+            result = new byte[length];
+        }else {
+            result = new byte[1024];
+        }
+        try{
+            ByteArrayInputStream rdb = null;
+            int read = rdb.read(result);
+
+        }catch (Exception e) {
+
+        }finally {
+
+        }
+        List<Byte> ll = new ArrayList<>();
+        List<byte[]> bytes = Arrays.asList(result);
+
+
+        return result;
     }
 }
--- src/test/java/com/cedis/core/rdb/rdbParseTest.java
+++ src/test/java/com/cedis/core/rdb/rdbParseTest.java
@@ -4,6 +4,13 @@ import com.cedis.core.old.ParseRDB;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -30,5 +37,33 @@ public class rdbParseTest {
             entry = rdb.next();
         }
         rdb.close();
+    }
+
+    @Test
+    public void rdbReadTest() throws Exception {
+        String filePath = "D:\\dump.rdb";
+        //Parser parser = new Parser();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        FileChannel channel = new RandomAccessFile(filePath,"r").getChannel();
+        //byteBuffer.limit(2);
+        int read = channel.read(byteBuffer);
+
+        if (read != -1) {
+            byte[] result = new byte[5];
+            byte[] array = byteBuffer.get(result, 0, 5).array();
+            System.out.println("result:" + new String(array));
+            byte[] result1 = new byte[8];
+            byteBuffer.get(result1, 0, 8);
+            System.out.println("result1:" + new String(result1));
+        }
+
+//        while (channel.read(allocate) != -1) {
+//            long position = channel.position();
+//            allocate.limit()
+//            allocate.clear();
+//            byte[] array = allocate.array();
+//            System.out.println("result:" + new String(array));
+//            System.out.println("position:" + position);
+//        }
     }
 }
